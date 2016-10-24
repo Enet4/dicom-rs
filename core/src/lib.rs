c9069c454fe11da43ff8169c9a2f0b8cec332ddb
Eduardo Pinho <enet4mikeenet@gmail.com>
2016-10-24 14:55:09 +0100

    Mark unused_qualifications as a warning

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,8 +1,8 @@
 #![crate_type = "lib"]
 #![deny(missing_debug_implementations, missing_copy_implementations,
         trivial_casts, trivial_numeric_casts, unsafe_code, unstable_features,
-        unused_import_braces, unused_qualifications)]
-#![warn(missing_docs)]
+        unused_import_braces)]
+#![warn(missing_docs, unused_qualifications)]
 
 //! This is a base library for dealing with DICOM information and communication.
 //!
