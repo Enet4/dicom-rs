714dc16df4d69bed377ebed0762807b604d85d25
Eduardo Pinho <enet4mikeenet@gmail.com>
2019-09-23 22:19:05 +0100

    Introduce TransferSyntaxIndex
    - trait for the TS registry
    - implement it, adapt existing code

--- transfer-syntax-registry/tests/submit.rs
+++ transfer-syntax-registry/tests/submit.rs
@@ -1,7 +1,7 @@
 //! Independent test for testing that submitting a TS in a separate crate work.
 use dicom_encoding::submit_transfer_syntax;
-use dicom_encoding::transfer_syntax::{Codec, DataRWAdapter, Endianness, TransferSyntax};
-use dicom_transfer_syntax_registry::get_registry;
+use dicom_encoding::transfer_syntax::{Codec, DataRWAdapter, Endianness, TransferSyntax, TransferSyntaxIndex};
+use dicom_transfer_syntax_registry::TransferSyntaxRegistry;
 use std::io::{Read, Write};
 
 /// this would, in theory, provide a dataset adapter
@@ -40,10 +40,8 @@ submit_transfer_syntax! {
 
 #[test]
 fn contains_dummy_ts() {
-    let registry = get_registry();
-
     // contains our dummy TS, and claims to be fully supported
-    let ts = registry.get("1.2.840.10008.9999.9999");
+    let ts = TransferSyntaxRegistry.get("1.2.840.10008.9999.9999");
     assert!(ts.is_some());
     let ts = ts.unwrap();
     assert_eq!(ts.uid(), "1.2.840.10008.9999.9999");
