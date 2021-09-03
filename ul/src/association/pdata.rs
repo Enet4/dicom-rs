a69cd90766985f217461a78bd0a4fe1fa53cdf72
Eduardo Pinho <enet4mikeenet@gmail.com>
2021-09-03 15:23:07 +0100

    Fix PDataWriter for large payloads (#164)
    - fix `dispatch_excess_data`
       - the routine that copies the rest of the bytes
         does not work if the remaining data is longer than max_data_len
    - refactor code a bit, add more documentation
    - update pdata writing test to cover consistency in large PDUs
       - the bug could only be detected when sending 3 or more PDUs
    - add public finish method
      which consumes the writer and finishes the PData stream
    - add doctest example of PDataWriter use

--- ul/src/association/pdata.rs
+++ ul/src/association/pdata.rs
@@ -1,5 +1,7 @@
 use std::io::Write;
 
+use crate::pdu::reader::PDU_HEADER_SIZE;
+
 /// A P-Data value writer.
 ///
 /// This exposes an API to iteratively construct and send Data messages
@@ -8,14 +10,47 @@ use std::io::Write;
 /// will automatically split the incoming bytes
 /// into separate PDUs if they do not fit in a single one.
 ///
+/// # Example
+///
 /// Use an association's `send_pdata` method
 /// to create a new P-Data value writer.
+///
+/// ```no_run
+/// # use std::io::Write;
+/// # use dicom_ul::association::{ClientAssociationOptions, PDataWriter};
+/// # use dicom_ul::pdu::{Pdu, PDataValue, PDataValueType};
+/// # fn command_data() -> Vec<u8> { unimplemented!() }
+/// # fn dicom_data() -> &'static [u8] { unimplemented!() }
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// let mut association = ClientAssociationOptions::new()
+///    .establish("129.168.0.5:104")?;
+///
+/// let presentation_context_id = association.presentation_contexts()[0].id;
+///
+/// // send a command first
+/// association.send(&Pdu::PData {
+///     data: vec![PDataValue {
+///     presentation_context_id,
+///     value_type: PDataValueType::Command,
+///         is_last: true,
+///         data: command_data(),
+///     }],
+/// });
+///
+/// // then send a DICOM object which may be split into multiple PDUs
+/// let mut pdata = association.send_pdata(presentation_context_id);
+/// pdata.write_all(dicom_data())?;
+/// pdata.finish()?;
+///
+/// let pdu_ac = association.receive()?;
+/// # Ok(())
+/// # }
 #[must_use]
 pub struct PDataWriter<W: Write> {
     buffer: Vec<u8>,
     stream: W,
     presentation_context_id: u8,
-    max_data_length: u32,
+    max_data_len: u32,
 }
 
 impl<W> PDataWriter<W>
@@ -23,31 +58,37 @@ where
     W: Write,
 {
     /// Construct a new P-Data value writer.
+    ///
+    /// `max_pdu_length` is the maximum value of the PDU-length property.
     pub(crate) fn new(stream: W, presentation_context_id: u8, max_pdu_length: u32) -> Self {
         let max_data_length = calculate_max_data_len_single(max_pdu_length);
         PDataWriter {
             stream,
             presentation_context_id,
-            max_data_length,
+            max_data_len: max_data_length,
             buffer: Vec::with_capacity(max_data_length as usize),
         }
     }
 
-    /// Send the header of a single P-Data PDU,
-    /// containing a single data fragment.
-    fn send_pdata_header(&mut self, data_len: u32, is_last: bool) -> std::io::Result<()> {
-        let mut message_header = 0x00;
-        if is_last {
-            message_header |= 0x02;
-        }
+    /// Declare to have finished sending P-Data fragments,
+    /// thus emitting the last P-Data fragment PDU.
+    ///
+    /// This is also done automatically once the P-Data writer is dropped.
+    pub fn finish(mut self) -> std::io::Result<()> {
+        self.finish_impl()?;
+        Ok(())
+    }
 
-        let pdu_len_bytes = (data_len + 2 + 4).to_be_bytes();
-        let data_len_bytes = (data_len + 2).to_be_bytes();
-        let header = [
+    fn create_pdata_header(&self, data_len: u32, is_last: bool) -> [u8; 12] {
+        let pdu_len = data_len + 4 + 2;
+        let pdv_data_len = data_len + 2;
+        let pdu_len_bytes = pdu_len.to_be_bytes();
+        let data_len_bytes = pdv_data_len.to_be_bytes();
+        [
             // PDU-type + reserved byte
             0x04,
             0x00,
-            // full PDU length
+            // full PDU length (minus PDU type and reserved byte)
             pdu_len_bytes[0],
             pdu_len_bytes[1],
             pdu_len_bytes[2],
@@ -60,37 +101,63 @@ where
             // presentation context id
             self.presentation_context_id,
             // message control header
-            message_header,
-        ];
-
-        self.stream.write_all(&header)
+            if is_last { 0x02 } else { 0x00 },
+        ]
     }
 
-    fn finish(&mut self) -> std::io::Result<()> {
+    /// Send the header of a single P-Data PDU,
+    /// containing a single data fragment.
+    fn send_pdata_header(&mut self, data_len: u32, is_last: bool) -> std::io::Result<()> {
+        self.stream
+            .write_all(&self.create_pdata_header(data_len, is_last))
+    }
+
+    fn finish_impl(&mut self) -> std::io::Result<()> {
         self.dispatch_excess_data()?;
         if !self.buffer.is_empty() {
             // send last PDU
             self.send_pdata_header(self.buffer.len() as u32, true)?;
             self.stream.write_all(&self.buffer[..])?;
+            // clear buffer so that subsequent calls to `finish_impl`
+            // do not send any more PDUs
             self.buffer.clear();
         }
         Ok(())
     }
 
-    fn dispatch_excess_data(&mut self) -> std::io::Result<()> {
-        while self.buffer.len() > self.max_data_length as usize {
-            // send PDU now
-            self.send_pdata_header(self.max_data_length, false)?;
-            let data = &self.buffer[0..self.max_data_length as usize];
-            self.stream.write_all(data)?;
+    /// Use the current state of the buffer to send new PDUs
+    ///
+    /// Pre-condition:
+    /// buffer must have enough data for one full P-Data-tf PDU
+    fn dispatch_pdus(&mut self) -> std::io::Result<()> {
+        debug_assert!(self.buffer.len() >= self.max_data_len as usize);
+        // send PDU now
 
-            // shift the remaining contents to the beginning of the buffer
-            let (p1, p2) = (&mut self.buffer[..]).split_at_mut(self.max_data_length as usize);
-            for (e1, e2) in std::iter::Iterator::zip(p1.iter_mut(), p2.iter()) {
-                *e1 = *e2;
+        let mut acc_len = 0;
+        for chunk in self.buffer.chunks(self.max_data_len as usize) {
+            if chunk.len() < self.max_data_len as usize {
+                break;
             }
-            self.buffer
-                .truncate(self.buffer.len() - self.max_data_length as usize);
+            let header = self.create_pdata_header(self.max_data_len, false);
+            self.stream.write_all(&header)?;
+            self.stream.write_all(chunk)?;
+            acc_len += chunk.len();
+        }
+
+        // shift the remaining contents to the beginning of the buffer
+        // (guaranteed to be shorter than `self.max_data_len`)
+        let (p1, p2) = (&mut self.buffer[..]).split_at_mut(acc_len);
+        for (e1, e2) in std::iter::Iterator::zip(p1.iter_mut(), p2.iter()) {
+            *e1 = *e2;
+        }
+        self.buffer.truncate(self.buffer.len() - acc_len as usize);
+
+        Ok(())
+    }
+
+    fn dispatch_excess_data(&mut self) -> std::io::Result<()> {
+        while self.buffer.len() > self.max_data_len as usize {
+            self.dispatch_pdus()?;
         }
         Ok(())
     }
@@ -112,34 +179,36 @@ where
     }
 }
 
+/// With the P-Data writer dropped,
+/// this `Drop` implementation
+/// will construct and emit the last P-Data fragment PDU
+/// if there is any data left to send.
 impl<W> Drop for PDataWriter<W>
 where
     W: Write,
 {
     fn drop(&mut self) {
-        let _ = self.finish();
+        let _ = self.finish_impl();
     }
 }
 
-/// Determine the maximum length of actual data
+/// Determine the maximum length of actual PDV data
 /// when encapsulated in a PDU with the given length property.
 /// Does not account for the first 2 bytes (type + reserved).
 #[inline]
 fn calculate_max_data_len_single(pdu_len: u32) -> u32 {
-    pdu_len
-    // data length
-    - 4
-    // control header
-    - 2
+    // data length: 4 bytes
+    // control header: 2 bytes
+    pdu_len - PDU_HEADER_SIZE
 }
 
 #[cfg(test)]
 mod tests {
     use std::io::Write;
 
+    use crate::pdu::reader::{read_pdu, MINIMUM_PDU_SIZE, PDU_HEADER_SIZE};
     use crate::pdu::PDataValueType;
     use crate::pdu::Pdu;
-    use crate::pdu::{read_pdu, reader::MINIMUM_PDU_SIZE};
 
     use super::PDataWriter;
 
@@ -179,8 +248,7 @@ mod tests {
     fn test_write_large_pdata_and_finish() {
         let presentation_context_id = 32;
 
-        let my_data: Vec<_> = (0..6000).map(|x| x as u8).collect();
-        assert_eq!(my_data.len(), 6000);
+        let my_data: Vec<_> = (0..9000).map(|x: u32| x as u8).collect();
 
         let mut buf = Vec::new();
         {
@@ -190,32 +258,63 @@ mod tests {
         }
 
         let mut cursor = &buf[..];
-        let same_pdu_1 = read_pdu(&mut cursor, MINIMUM_PDU_SIZE, true).unwrap();
-        let same_pdu_2 = read_pdu(&mut cursor, MINIMUM_PDU_SIZE, true).unwrap();
+        let pdu_1 = read_pdu(&mut cursor, MINIMUM_PDU_SIZE, true).unwrap();
+        let pdu_2 = read_pdu(&mut cursor, MINIMUM_PDU_SIZE, true).unwrap();
+        let pdu_3 = read_pdu(&mut cursor, MINIMUM_PDU_SIZE, true).unwrap();
 
         // concatenate data chunks, compare with all data
 
-        match (same_pdu_1, same_pdu_2) {
-            (Pdu::PData { data: data_1 }, Pdu::PData { data: data_2 }) => {
+        match (pdu_1, pdu_2, pdu_3) {
+            (
+                Pdu::PData { data: data_1 },
+                Pdu::PData { data: data_2 },
+                Pdu::PData { data: data_3 },
+            ) => {
+                assert_eq!(data_1.len(), 1);
                 let data_1 = &data_1[0];
+                assert_eq!(data_2.len(), 1);
                 let data_2 = &data_2[0];
+                assert_eq!(data_3.len(), 1);
+                let data_3 = &data_3[0];
 
                 // check that these two PDUs are consistent
                 assert_eq!(data_1.value_type, PDataValueType::Data);
                 assert_eq!(data_2.value_type, PDataValueType::Data);
                 assert_eq!(data_1.presentation_context_id, presentation_context_id);
                 assert_eq!(data_2.presentation_context_id, presentation_context_id);
-                assert_eq!(data_1.data.len() + data_2.data.len(), 6000);
+
+                // check expected lengths
+                assert_eq!(
+                    data_1.data.len(),
+                    (MINIMUM_PDU_SIZE - PDU_HEADER_SIZE) as usize
+                );
+                assert_eq!(
+                    data_2.data.len(),
+                    (MINIMUM_PDU_SIZE - PDU_HEADER_SIZE) as usize
+                );
+                assert_eq!(data_3.data.len(), 820);
+
+                // check data consistency
+                assert_eq!(
+                    &data_1.data[..],
+                    (0..MINIMUM_PDU_SIZE - PDU_HEADER_SIZE).map(|x| x as u8).collect::<Vec<_>>()
+                );
+                assert_eq!(
+                    data_1.data.len() + data_2.data.len() + data_3.data.len(),
+                    9000
+                );
 
                 let data_1 = &data_1.data;
                 let data_2 = &data_2.data;
+                let data_3 = &data_3.data;
 
                 let mut all_data: Vec<u8> = Vec::new();
                 all_data.extend(data_1);
                 all_data.extend(data_2);
+                all_data.extend(data_3);
                 assert_eq!(all_data, my_data);
             }
-            x => panic!("Expected two PDatas, got {:?}", x),
+            x => panic!("Expected 3 PDatas, got {:?}", x),
         }
 
         assert_eq!(cursor.len(), 0);
