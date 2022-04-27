69b47e6f1f2e6936494894bc1cc85c92db0d8139
Eduardo Pinho <enet4mikeenet@gmail.com>
2022-04-27 17:40:41 +0100

    Install default tracing subscriber in some tools
    - toimage
    - fromimage

--- fromimage/src/main.rs
+++ fromimage/src/main.rs
@@ -76,6 +76,12 @@ where
 }
 
 fn main() {
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::FmtSubscriber::new(),
+    ).unwrap_or_else(|e| {
+        report(&e);
+    });
+
     let App {
         dcm_file,
         img_file,
