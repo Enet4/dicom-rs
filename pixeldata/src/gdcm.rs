cc12a78e3e5a1ac2bcb14fd6f495affb0fec4f76
Lukas Winkler <derwinlu+git@gmail.com>
2022-08-17 17:41:57 +0200

    Remove slicing of primitive pixel data

--- pixeldata/src/gdcm.rs
+++ pixeldata/src/gdcm.rs
@@ -103,11 +103,7 @@ where
             }
             Value::Primitive(p) => {
                 // Non-encoded, just return the pixel data of the first frame
-                let total_bytes = rows as usize
-                    * cols as usize
-                    * samples_per_pixel as usize
-                    * (bits_allocated as usize / 8);
-                p.to_bytes()[0..total_bytes].to_vec()
+                p.to_bytes().to_vec()
             }
             Value::Sequence { items: _, size: _ } => InvalidPixelDataSnafu.fail()?,
         };
