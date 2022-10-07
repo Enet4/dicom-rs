b607dc9e57da89fec540c56235709c8c94422022
Eduardo Pinho <enet4mikeenet@gmail.com>
2022-10-07 20:19:35 +0100

    [ul] elide lifetime in uid::trim_uid

--- ul/src/association/uid.rs
+++ ul/src/association/uid.rs
@@ -2,7 +2,7 @@
 
 use std::borrow::Cow;
 
-pub(crate) fn trim_uid<'a>(uid: Cow<'a, str>) -> Cow<'a, str> {
+pub(crate) fn trim_uid(uid: Cow<str>) -> Cow<str> {
     if uid.ends_with('\0') {
         Cow::Owned(uid.trim_end_matches(|c| c == '\0').to_string())
     } else {
