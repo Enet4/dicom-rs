95cd2a9a0af9f1eb4bacb991d38d10eef4bf5878
Douglas Yau <dy@dicomserver.net>
2022-11-27 04:29:25 -0600

    Instead of throwing an error when too many fragments are requested, use the minimum posible size. Consume the input vector intead of using a reference.

--- pixeldata/src/encapsulation.rs
+++ pixeldata/src/encapsulation.rs
@@ -3,61 +3,57 @@ use dicom_core::DataDictionary;
 use dicom_object::mem::InMemFragment;
 use dicom_object::InMemDicomObject;
 use snafu::prelude::*;
-
-#[derive(Debug, Snafu)]
-pub enum EncapsulationError {
-    TooManyFragments,
-}
-/// Encapsulate the pixel data of the frames. If frames > 1 then fragments is ignored and set to 1
-pub fn encapsulate<D>(
-    frames: &mut [Vec<u8>],
-    fragments: u32,
-) -> Result<Value<InMemDicomObject<D>, InMemFragment>, EncapsulationError>
+/// Encapsulate the pixel data of the frames. If frames > 1 then fragments is ignored and set to 1.
+/// If the calculated fragment size is less than 2 bytes, then it is set to 2 bytes
+pub fn encapsulate<'a, D>(
+    frames: Vec<Vec<u8>>,
+    number_of_fragments: u32,
+) -> Value<InMemDicomObject<D>, InMemFragment>
 where
     D: DataDictionary,
     D: Clone,
 {
-    let mut processed = C::new();
+    let mut frames = frames;
+    let number_of_fragments = if frames.len() > 1 { 1 } else { number_of_fragments };
     let mut bot = C::new();
-    let fragments = if frames.len() > 1 { 1 } else { fragments };
+    let mut fragments = C::new();
 
     frames
         .iter_mut()
-        .map(|data| {
-            let fragment_size = (data.len() as f32 / fragments as f32).ceil() as u32;
-            if fragment_size > 2 {
-                // Fragment size has to be even and 4 bytes in size
-                let fragment_size = {
-                    if fragment_size % 2 == 0 {
-                        fragment_size
-                    } else {
-                        fragment_size + 1
-                    }
-                } as u32;
-
-                let encapsulated_size = (fragment_size * fragments) as usize;
-                if encapsulated_size > data.len() {
-                    // Pad data of the last fragment
-                    data.resize(encapsulated_size, 0);
+        .flat_map(|data| {
+            // Calculate the fragment size. If it is less than 2 bytes, make it 2 bytes.
+            // Otherwise make it even.
+            let fragment_size = (data.len() as f32 / number_of_fragments as f32).ceil() as u32;
+            let fragment_size = if fragment_size > 2 {
+                if fragment_size % 2 == 0 {
+                    fragment_size
+                } else {
+                    fragment_size + 1
                 }
-                Ok(data.chunks_exact(fragment_size as usize))
             } else {
-                Err(EncapsulationError::TooManyFragments)
+                2u32
+            };
+
+            // Calculate the encapsulated size. If necessary pad the vector with zeroes so all the
+            // chunks have the same fragment_size
+            let encapsulated_size = (fragment_size * number_of_fragments) as usize;
+            if encapsulated_size > data.len() {
+                data.resize(encapsulated_size, 0);
             }
+
+            data.chunks_exact(fragment_size as usize)
         })
-        .collect::<Result<Vec<_>, EncapsulationError>>()?
-        .into_iter()
-        .flatten()
+        .map(|fragment| fragment.to_owned().to_vec())
         .fold(0, |acc, fragment| {
             let length = fragment.len() as u32;
-            processed.push(Vec::from(fragment));
             bot.push(acc as u32);
+            fragments.push((*fragment).to_vec());
 
             acc + length + 1
         });
 
-    Ok(Value::PixelSequence {
+    Value::PixelSequence {
         offset_table: bot,
-        fragments: processed,
-    })
+        fragments: fragments,
+    }
 }
