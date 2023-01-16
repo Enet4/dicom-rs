ac60fdb93ff5a49dbffa6a7f8e72d2f922ad6f1e
Douglas Yau <dy@dicomserver.net>
2023-01-16 15:23:38 -0600

    Use fragment_size instead of number_of_fragments to calculate the fragment size

--- pixeldata/src/encapsulation.rs
+++ pixeldata/src/encapsulation.rs
@@ -8,13 +8,13 @@ pub struct EncapsulatedPixels {
     offset_table: C<u32>,
     current_offset: u32,
     fragments: C<Vec<u8>>,
-    number_of_fragments: u32,
+    fragment_size: u32,
 }
 
 impl EncapsulatedPixels {
     /// Add a single frame to EncapsulatedPixels
     pub fn add_frame(&mut self, data: Vec<u8>) {
-        for fragment in fragment_frame(data, self.number_of_fragments) {
+        for fragment in fragment_frame(data, self.fragment_size) {
             let size = fragment.len() as u32;
             self.offset_table.push(self.current_offset);
             self.fragments.push((*fragment).to_vec());
@@ -23,18 +23,12 @@ impl EncapsulatedPixels {
     }
 
     /// Creates an empty EncapsulatedPixels struct
-    pub fn new(number_of_fragments: u32) -> Self {
-        let number_of_fragments = if number_of_fragments > 0 {
-            number_of_fragments
-        } else {
-            1u32
-        };
-
+    pub fn new(fragment_size: u32) -> Self {
         EncapsulatedPixels {
             offset_table: C::new(),
             current_offset: 0,
             fragments: C::new(),
-            number_of_fragments,
+            fragment_size,
         }
     }
 }
@@ -54,36 +48,36 @@ where
 impl From<Vec<Vec<u8>>> for EncapsulatedPixels {
     /// Create EncapsulatedPixels from a list of fragments and calculate the bot
     fn from(fragments: Vec<Vec<u8>>) -> Self {
-        let mut bot = C::with_capacity(fragments.len());
+        let mut offset_table = C::with_capacity(fragments.len());
         let mut current_offset = 0u32;
         for fragment in &fragments {
-            bot.push(current_offset);
+            offset_table.push(current_offset);
             current_offset += fragment.len() as u32;
         }
 
         EncapsulatedPixels {
-            offset_table: bot,
+            offset_table,
             current_offset,
             fragments: fragments.into(),
-            number_of_fragments: 1,
+            fragment_size: 0,
         }
     }
 }
 
 /// Create the fragments for a single frame. It returns a list with the fragments.
-pub fn fragment_frame(data: Vec<u8>, number_of_fragments: u32) -> Vec<Vec<u8>> {
-    // Calculate the fragment size. If it is less than 2 bytes, make it 2 bytes.
-    // Otherwise make it even.
-    let fragment_size = (data.len() as f32 / number_of_fragments as f32).ceil() as u32;
-    let fragment_size = if fragment_size > 2 {
-        if fragment_size % 2 == 0 {
-            fragment_size
-        } else {
-            fragment_size + 1
-        }
+pub fn fragment_frame(data: Vec<u8>, fragment_size: u32) -> Vec<Vec<u8>> {
+    let fragment_size: u32 = if fragment_size == 0 {
+        data.len() as u32
     } else {
-        2u32
+        fragment_size
     };
+
+    let fragment_size = if fragment_size % 2 == 0 {
+        fragment_size
+    } else {
+        fragment_size + 1
+    };
+
     let number_of_fragments = (data.len() as f32 / fragment_size as f32).ceil() as u32;
 
     // Calculate the encapsulated size. If necessary pad the vector with zeroes so all the
@@ -101,11 +95,11 @@ pub fn fragment_frame(data: Vec<u8>, number_of_fragments: u32) -> Vec<Vec<u8>> {
 
 /// Encapsulate the pixel data of the frames. If frames > 1 then fragments is ignored and set to 1.
 /// If the calculated fragment size is less than 2 bytes, then it is set to 2 bytes
-pub fn encapsulate(frames: Vec<Vec<u8>>, number_of_fragments: u32) -> EncapsulatedPixels {
+pub fn encapsulate(frames: Vec<Vec<u8>>, fragment_size: u32) -> EncapsulatedPixels {
     let number_of_fragments = if frames.len() > 1 {
-        1
+        0
     } else {
-        number_of_fragments
+        fragment_size
     };
     let mut encapsulated_data = EncapsulatedPixels::new(number_of_fragments);
 
