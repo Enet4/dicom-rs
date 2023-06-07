3ddb22fc603d031cc7faefc228843c45c3df5dec
Eduardo Pinho <enet4mikeenet@gmail.com>
2023-06-07 18:32:07 +0100

    [ts-registry] always use inventory from dicom-encoding
    - remove inventory at this level
    - re-export inventory from dicom_encoding
    - add another integration test for submitting a stub TS

---
+++ transfer-syntax-registry/tests/submit_pixel_stub.rs
@@ -0,0 +1,36 @@
+//! Independent test for submission of a dummy TS implementation
+//! withour adapters.
+//!
+//! Only applicable to the inventory-based registry.
+#![cfg(feature = "inventory-registry")]
+
+use dicom_encoding::{
+    submit_transfer_syntax, AdapterFreeTransferSyntax, Codec, Endianness, TransferSyntaxIndex,
+};
+use dicom_transfer_syntax_registry::TransferSyntaxRegistry;
+
+// install this dummy as a private transfer syntax
+submit_transfer_syntax! {
+    AdapterFreeTransferSyntax::new(
+        "1.2.840.10008.999.9999.99999",
+        "Dummy Little Endian",
+        Endianness::Little,
+        true,
+        Codec::EncapsulatedPixelData,
+    )
+}
+
+#[test]
+fn contains_stub_ts() {
+    // contains our stub TS, not fully fully supported,
+    // but enough to read some datasets
+    let ts = TransferSyntaxRegistry.get("1.2.840.10008.999.9999.99999");
+    assert!(ts.is_some());
+    let ts = ts.unwrap();
+    assert_eq!(ts.uid(), "1.2.840.10008.999.9999.99999");
+    assert_eq!(ts.name(), "Dummy Little Endian");
+    assert!(!ts.fully_supported());
+    assert!(ts.unsupported_pixel_encapsulation());
+    // can obtain a data set decoder
+    assert!(ts.decoder().is_some());
+}
