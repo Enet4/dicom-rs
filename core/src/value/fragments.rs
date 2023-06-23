e8cdfee8a236d52cd6dc577b73079e43c65ff51d
Douglas Yau <dy@dicomserver.net>
2023-06-23 15:02:17 -0600

    Document dicom_core::fragments

--- core/src/value/fragments.rs
+++ core/src/value/fragments.rs
@@ -1,8 +1,36 @@
-use crate::value::{PixelFragmentSequence, C};
+//! Handle pixel encapsulation into fragments
+use crate::value::{InMemFragment, PixelFragmentSequence, C};
 
+/// Represents the fragments of a single frame. [PixelFragmentSequence] can be generated from a list
+/// of [Fragments]. In case of multi frame a list of frames composed by 1 fragment is expected.
+///
+/// The frames can be independently processed, so parallel execution is possible.
+///
+/// # Example
+/// ```
+/// // Frames are represented as Vec<Vec<u8>>
+/// use dicom_core::{DataElement, Tag};
+/// use dicom_core::header::EmptyObject;
+/// use dicom_core::value::Value::PixelSequence;
+/// use dicom_core::value::fragments::Fragments;
+/// use dicom_core::value::InMemFragment;
+/// use dicom_core::VR::OB;
+///
+/// // Single 512x512 frame
+/// let frames = vec![vec![0; 262144]];
+/// let fragments = frames
+///     .into_iter()
+///     .map(|frame| Fragments::new(frame, 0))
+///     .collect::<Vec<Fragments>>();
+///
+/// let element = DataElement::new(Tag(0x7FE0, 0x0008), OB, PixelSequence::<EmptyObject, InMemFragment>(fragments.into()));
+/// ```
+///
+/// From this last example, it is possible to extend it to implement a pipeline, and even use rayon
+/// process the frames.
 #[derive(Debug)]
 pub struct Fragments {
-    fragments: Vec<Vec<u8>>,
+    fragments: Vec<InMemFragment>,
 }
 
 impl Fragments {
@@ -32,7 +60,7 @@ impl Fragments {
         let fragments = data
             .chunks_exact(fragment_size as usize)
             .map(|fragment| fragment.to_vec())
-            .collect::<Vec<Vec<u8>>>();
+            .collect::<Vec<InMemFragment>>();
 
         Fragments { fragments }
     }
