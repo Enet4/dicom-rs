e8a244f02731229569d3ba9c68e1b1e2bb8a15ef
Eduardo Pinho <enet4mikeenet@gmail.com>
2023-09-13 19:00:21 +0100

    [toimage] Implement --unwrap option
    - extract pixel data as is,
      without decoding
    - tweak dependencies

--- toimage/src/main.rs
+++ toimage/src/main.rs
@@ -1,11 +1,13 @@
 //! A CLI tool for converting a DICOM image file
 //! into a general purpose image file (e.g. PNG).
-use std::path::PathBuf;
+use std::{borrow::Cow, path::PathBuf};
 
 use clap::Parser;
+use dicom_core::prelude::*;
+use dicom_dictionary_std::{tags, uids};
 use dicom_object::open_file;
 use dicom_pixeldata::{ConvertOptions, PixelDecoder};
-use snafu::{Report, ResultExt, Whatever};
+use snafu::{Report, ResultExt, Snafu, Whatever};
 use tracing::{error, Level};
 
 /// Convert a DICOM file into an image file
@@ -32,24 +34,63 @@ struct App {
     #[arg(long = "16bit", conflicts_with = "force_8bit")]
     force_16bit: bool,
 
+    /// Output the raw pixel data instead of decoding it
+    #[arg(
+        long = "unwrap",
+        conflicts_with = "force_8bit",
+        conflicts_with = "force_16bit"
+    )]
+    unwrap: bool,
+
     /// Print more information about the image and the output file
     #[arg(short = 'v', long = "verbose")]
     verbose: bool,
 }
 
+#[derive(Debug, Snafu)]
+enum Error {
+    #[snafu(display("could not read DICOM file {}", path.display()))]
+    ReadFile {
+        source: dicom_object::ReadError,
+        path: PathBuf,
+    },
+    /// failed to decode pixel data
+    DecodePixelData { source: dicom_pixeldata::Error },
+    /// failed to convert pixel data to image
+    ConvertImage { source: dicom_pixeldata::Error },
+    /// failed to save image to file
+    SaveImage {
+        source: dicom_pixeldata::image::ImageError,
+    },
+    /// failed to save pixel data to file
+    SaveData { source: std::io::Error },
+    /// Unexpected DICOM pixel data as data set sequence
+    UnexpectedPixelData,
+}
+
+impl Error {
+    fn to_exit_code(&self) -> i32 {
+        match self {
+            Error::ReadFile { .. } => -1,
+            Error::DecodePixelData { .. } => -2,
+            Error::ConvertImage { .. } => -3,
+            Error::SaveData { .. } => -4,
+            Error::SaveImage { .. } => -4,
+            Error::UnexpectedPixelData => -7,
+        }
+    }
+}
+
 fn main() {
-    let App {
-        file,
-        output,
-        frame_number,
-        verbose,
-        force_8bit,
-        force_16bit,
-    } = App::parse();
+    let args = App::parse();
 
     tracing::subscriber::set_global_default(
         tracing_subscriber::FmtSubscriber::builder()
-            .with_max_level(if verbose { Level::DEBUG } else { Level::INFO })
+            .with_max_level(if args.verbose {
+                Level::DEBUG
+            } else {
+                Level::INFO
+            })
             .finish(),
     )
     .whatever_context("Could not set up global logging subscriber")
@@ -57,55 +98,134 @@ fn main() {
         eprintln!("[ERROR] {}", Report::from_error(e));
     });
 
-    let output = output.unwrap_or_else(|| {
-        let mut path = file.clone();
-        path.set_extension("png");
-        path
-    });
-
-    let obj = open_file(&file).unwrap_or_else(|e| {
+    run(args).unwrap_or_else(|e| {
+        let code = e.to_exit_code();
         error!("{}", Report::from_error(e));
-        std::process::exit(-1);
+        std::process::exit(code);
     });
+}
 
-    let pixel = obj.decode_pixel_data().unwrap_or_else(|e| {
-        error!("{}", Report::from_error(e));
-        std::process::exit(-2);
-    });
+fn run(args: App) -> Result<(), Error> {
+    let App {
+        file,
+        output,
+        frame_number,
+        force_8bit,
+        force_16bit,
+        unwrap,
+        verbose,
+    } = args;
 
-    if verbose {
-        println!(
-            "{}x{}x{} image, {}-bit",
-            pixel.columns(),
-            pixel.rows(),
-            pixel.samples_per_pixel(),
-            pixel.bits_stored()
-        );
-    }
+    let obj = open_file(&file).with_context(|_| ReadFileSnafu { path: file.clone() })?;
 
-    let mut options = ConvertOptions::new();
+    if unwrap {
+        let output = output.unwrap_or_else(|| {
+            let mut path = file.clone();
 
-    if force_16bit {
-        options = options.force_16bit();
-    } else if force_8bit {
-        options = options.force_8bit();
-    }
-
-    let image = pixel
-        .to_dynamic_image_with_options(frame_number, &options)
-        .unwrap_or_else(|e| {
-            error!("{}", Report::from_error(e));
-            std::process::exit(-3);
+            // try to identify a better extension for this file
+            // based on transfer syntax
+            match obj.meta().transfer_syntax() {
+                uids::JPEG_BASELINE8_BIT | uids::JPEG_LOSSLESS | uids::JPEG_LOSSLESS_SV1 => {
+                    path.set_extension("jpg");
+                }
+                uids::JPEG_EXTENDED12_BIT => {
+                    path.set_extension("jpe");
+                }
+                uids::JPEG2000
+                | uids::JPEG2000MC
+                | uids::JPEG2000MC_LOSSLESS
+                | uids::JPEG2000_LOSSLESS => {
+                    path.set_extension("jp2");
+                }
+                _ => {
+                    path.set_extension("data");
+                }
+            }
+            path
         });
 
-    image.save(&output).unwrap_or_else(|e| {
-        error!("{}", Report::from_error(e));
-        std::process::exit(-4);
-    });
+        let pixeldata = obj.get(tags::PIXEL_DATA).unwrap_or_else(|| {
+            error!("DICOM file has no pixel data");
+            std::process::exit(-2);
+        });
 
-    if verbose {
-        println!("Image saved to {}", output.display());
+        let out_data = match pixeldata.value() {
+            DicomValue::PixelSequence(seq) => {
+                let number_of_frames = match obj.get(tags::NUMBER_OF_FRAMES) {
+                    Some(elem) => elem.to_int::<u32>().unwrap_or_else(|e| {
+                        tracing::warn!("Invalid Number of Frames: {}", e);
+                        1
+                    }),
+                    None => 1,
+                };
+
+                if number_of_frames as usize == seq.fragments().len() {
+                    // frame-to-fragment mapping is 1:1
+
+                    // get fragment containing our frame
+                    let fragment =
+                        seq.fragments()
+                            .get(frame_number as usize)
+                            .unwrap_or_else(|| {
+                                error!("Frame number {} is out of range", frame_number);
+                                std::process::exit(-2);
+                            });
+
+                    Cow::Borrowed(&fragment[..])
+                } else {
+                    // accumulate frame's pixel data into a single buffer
+                    todo!("unwrapping encapsulated pixel data with multiple fragments")
+                }
+            }
+            DicomValue::Primitive(v) => {
+                // temporary impl: grab and dump all of it as bytes
+                v.to_bytes()
+            }
+            _ => {
+                return UnexpectedPixelDataSnafu.fail();
+            }
+        };
+
+        std::fs::write(&output, &out_data).context(SaveDataSnafu)?;
+    } else {
+        let output = output.unwrap_or_else(|| {
+            let mut path = file.clone();
+            path.set_extension("png");
+            path
+        });
+
+        let pixel = obj.decode_pixel_data().context(DecodePixelDataSnafu)?;
+
+        if verbose {
+            println!(
+                "{}x{}x{} image, {}-bit",
+                pixel.columns(),
+                pixel.rows(),
+                pixel.samples_per_pixel(),
+                pixel.bits_stored()
+            );
+        }
+
+        let mut options = ConvertOptions::new();
+
+        if force_16bit {
+            options = options.force_16bit();
+        } else if force_8bit {
+            options = options.force_8bit();
+        }
+
+        let image = pixel
+            .to_dynamic_image_with_options(frame_number, &options)
+            .context(ConvertImageSnafu)?;
+
+        image.save(&output).context(SaveImageSnafu)?;
+
+        if verbose {
+            println!("Image saved to {}", output.display());
+        }
     }
+
+    Ok(())
 }
 
 #[cfg(test)]
