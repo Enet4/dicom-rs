2844d942437cec0c115bd0664f60a49c605f5e11
Eduardo Pinho <enet4mikeenet@gmail.com>
2023-10-29 16:17:04 +0000

    Add JPEG 2000 decoding support via jpeg2k
    - [ts-registry] add features openjp2 and openjpeg-sys
       - map to the backend impl of JPEG 2000 via jpeg2k
    - [ts-registry] add JPEG 2000 pixel data adapter with decoding support
    - [pixeldata] expose more transfer-syntax-registry features
    - [pixeldata] adjust jpeg decoding test cases according to support
    - [toimage] relocate dicom-pixeldata/native to default features
      so that native implementations can be disabled at will

--- transfer-syntax-registry/src/adapters/mod.rs
+++ transfer-syntax-registry/src/adapters/mod.rs
@@ -4,15 +4,27 @@
 //! can be added via Cargo features.
 //!
 //! - [`jpeg`](jpeg) provides native JPEG decoding
-//!   (baseline and lossless).
+//!   (baseline and lossless)
+//!   and encoding (baseline).
 //!   Requires the `jpeg` feature,
 //!   enabled by default.
-//! - [`rle_lossless`](rle_lossless) provides RLE lossless decoding.
+//! - [`jpeg2k`](jpeg2k) contains JPEG 2000 support,
+//!   which is currently available through [OpenJPEG].
+//!   The `openjp2` feature provides native JPEG 2000 decoding
+//!   via the [Rust port of OpenJPEG][OpenJPEG-rs].
+//!   Alternatively, enable the `openjpeg-sys` feature
+//!   to statically link to the OpenJPEG reference implementation.
+//!   `openjp2` is enabled by default.
+//! - [`rle_lossless`](rle_lossless) provides native RLE lossless decoding.
 //!   Requires the `rle` feature,
 //!   enabled by default.
-
+//! 
+//! [OpenJPEG]: https://github.com/uclouvain/openjpeg
+//! [OpenJPEG-rs]: https://crates.io/crates/openjp2
 #[cfg(feature = "jpeg")]
 pub mod jpeg;
+#[cfg(any(feature = "openjp2", feature = "openjpeg-sys"))]
+pub mod jpeg2k;
 #[cfg(feature = "rle")]
 pub mod rle_lossless;
 
@@ -22,6 +34,11 @@ pub mod uncompressed;
 /// Enable the `jpeg` feature to use this module.
 #[cfg(not(feature = "jpeg"))]
 pub mod jpeg {}
+
+/// **Note:** This module is a stub.
+/// Enable either `openjp2` or `openjpeg-sys` to use this module.
+#[cfg(not(any(feature = "openjp2", feature = "openjpeg-sys")))]
+pub mod jpeg2k {}
 
 /// **Note:** This module is a stub.
 /// Enable the `rle` feature to use this module.
