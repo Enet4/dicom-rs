57d8907a1b587c16a1250d9f283ff6c7480c6092
Nathan Richman <naterichman@flywheel.io>
2023-11-19 09:38:53 -0600

    ENH: Add support for deflate writing
    * Note, not all paths lead to writing, for example using
      `InMemDicomObject.read_dataset_with_dict` would bypass deflate.
    * No support for writing right now

--- transfer-syntax-registry/src/deflate.rs
+++ transfer-syntax-registry/src/deflate.rs
@@ -1,8 +1,7 @@
 //! Implementation of Deflated Explicit VR Little Endian.
 use std::io::{Read, Write};
 
-use byteordered::Endianness;
-use dicom_encoding::{Codec, TransferSyntax, transfer_syntax::DataRWAdapter};
+use dicom_encoding::transfer_syntax::DataRWAdapter;
 use flate2;
 use flate2::Compression;
 
@@ -10,34 +9,27 @@ use flate2::Compression;
 #[derive(Debug)]
 pub struct FlateAdapter;
 
-/// **Fully implemented**: Deflated Explicit VR Little Endian
-pub const DEFLATED_EXPLICIT_VR_LITTLE_ENDIAN: TransferSyntax<FlateAdapter> = TransferSyntax::new(
-    "1.2.840.10008.1.2.1.99",
-    "Deflated Explicit VR Little Endian",
-    Endianness::Little,
-    true,
-    Codec::Dataset(FlateAdapter),
-);
-
 impl<R: 'static, W: 'static> DataRWAdapter<R, W> for FlateAdapter
 where
     R: Read,
     W: Write,
 {
-    type Reader = flate2::read::DeflateDecoder<R>;
-    type Writer = flate2::write::DeflateEncoder<W>;
+    // type Reader = Box<flate2::read::DeflateDecoder<R>>;
+    // type Writer = Box<flate2::write::DeflateEncoder<W>>;
+    type Reader = Box<dyn Read>;
+    type Writer = Box<dyn Write>;
 
     fn adapt_reader(&self, reader: R) -> Self::Reader
     where
         R: Read,
     {
-        flate2::read::DeflateDecoder::new(reader)
+        Box::new(flate2::read::DeflateDecoder::new(reader))
     }
 
     fn adapt_writer(&self, writer: W) -> Self::Writer
     where
         W: Write,
     {
-        flate2::write::DeflateEncoder::new(writer, Compression::fast())
+        Box::new(flate2::write::DeflateEncoder::new(writer, Compression::fast()))
     }
 }
