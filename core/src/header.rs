9997f9c4baaa9a1d3375ee63cd21278c1152c6d7
Eduardo Pinho <enet4mikeenet@gmail.com>
2024-03-19 19:23:07 +0000

    [core] Tweak qualification of impl Add<Length>

--- core/src/header.rs
+++ core/src/header.rs
@@ -1217,7 +1217,7 @@ impl PartialOrd<Length> for Length {
     }
 }
 
-impl ::std::ops::Add<Length> for Length {
+impl std::ops::Add<Length> for Length {
     type Output = Self;
 
     fn add(self, rhs: Length) -> Self::Output {
@@ -1235,7 +1235,7 @@ impl ::std::ops::Add<Length> for Length {
     }
 }
 
-impl ::std::ops::Add<i32> for Length {
+impl std::ops::Add<i32> for Length {
     type Output = Self;
 
     fn add(self, rhs: i32) -> Self::Output {
