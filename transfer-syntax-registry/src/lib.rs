62e5b0ea83755a47310f7c1500975207ef73fadf
Eduardo Pinho <enet4mikeenet@gmail.com>
2024-10-16 22:32:50 +0100

    [ts-registry] Update root crate docs
    with jpegxl

--- transfer-syntax-registry/src/lib.rs
+++ transfer-syntax-registry/src/lib.rs
@@ -62,13 +62,17 @@
 //! | JPEG 2000                     | Cargo feature `openjp2` or `openjpeg-sys` | x |
 //! | JPEG 2000 Part 2 Multi-component Image Compression (Lossless Only) | Cargo feature `openjp2` or `openjpeg-sys` | x |
 //! | JPEG 2000 Part 2 Multi-component Image Compression | Cargo feature `openjp2` or `openjpeg-sys` | x |
+//! | JPEG XL Lossless              | Cargo feature `jpegxl` | ✓ |
+//! | JPEG XL Recompression         | Cargo feature `jpegxl` | x |
+//! | JPEG XL                       | Cargo feature `jpegxl` | ✓ |
 //! | RLE Lossless                  | Cargo feature `rle` | x |
 //!
-//! Cargo features behind `native` (`jpeg`, `rle`)
-//! provide implementations that are written in pure Rust
-//! and are likely available in all supported platforms.
-//! However, a native implementation might not always be available,
-//! or alternative implementations may be preferred:
+//! Cargo features behind `native` (`jpeg`, `rle`) are added by default.
+//! They provide implementations that are written in pure Rust
+//! and are likely available in all supported platforms without issues.
+//! Additional codecs are opt-in by enabling Cargo features,
+//! for scenarios where a native implementation is not available,
+//! or alternative implementations are available.
 //!
 //! - `charls` provides support for JPEG-LS
 //!   by linking to the CharLS reference implementation,
@@ -81,7 +85,9 @@
 //!   Include `openjpeg-sys-threads` to build OpenJPEG with multithreading.
 //! - `openjp2` provides a binding to a computer-translated Rust port of OpenJPEG.
 //!   Due to the nature of this crate,
-//!   it does not work on all supported platforms.
+//!   it might not work on all modern platforms.
+//! - `jpegxl` adds JPEG XL support using `jxl-oxide` for decoding
+//!   and `zune-jpegxl` for encoding.
 //!
 //! Transfer syntaxes which are not supported,
 //! either due to being unable to read the data set
@@ -92,7 +98,6 @@
 //! if using the inventory-based registry.
 //!
 //! [inventory]: https://docs.rs/inventory/0.3.12/inventory
-
 use dicom_encoding::transfer_syntax::{
     AdapterFreeTransferSyntax as Ts, Codec, TransferSyntaxIndex,
 };
