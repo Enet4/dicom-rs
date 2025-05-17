f399b00d2bc5a9b73a2204ac2d9632e9c7a1a8f2
Eduardo Pinho <enet4mikeenet@gmail.com>
2025-05-17 16:47:23 +0100

    [object] Extend DicomObject
    - add associated type `LeafAttribute<'_>`
       - opens the possibility of the attribute type being different when doing a deeper look-up
    - add method `DicomObject::at`
    - extend documentation to advise when to use `DicomObject
    - fix impl of DicomObject for &FileDicomObject<O>
      -  it was not consulting like the non-ref counterpart

--- object/src/mem.rs
+++ object/src/mem.rs
@@ -139,6 +139,9 @@ where
     type Attribute<'a> = &'a Value<InMemDicomObject<D>, InMemFragment>
             where Self: 'a;
 
+    type LeafAttribute<'a> = &'a Value<InMemDicomObject<D>, InMemFragment>
+            where Self: 'a;
+
     #[inline]
     fn get_opt(&self, tag: Tag) -> Result<Option<Self::Attribute<'_>>> {
         let elem = InMemDicomObject::element_opt(self, tag)?;
@@ -165,6 +168,11 @@ where
         let elem = InMemDicomObject::element_by_name(self, name)?;
         Ok(elem.value())
     }
+
+    #[inline]
+    fn at(&self, selector: impl Into<AttributeSelector>) -> Result<Self::LeafAttribute<'_>, AtAccessError> {
+        InMemDicomObject::value_at(&self, selector)
+    }
 }
 
 impl<'s, D: 's> DicomObject for &'s InMemDicomObject<D>
@@ -173,6 +181,10 @@ where
     D: Clone,
 {
     type Attribute<'a> = &'a Value<InMemDicomObject<D>, InMemFragment>
+        where Self: 'a,
+        's: 'a;
+
+    type LeafAttribute<'a> = &'a Value<InMemDicomObject<D>, InMemFragment>
         where Self: 'a,
         's: 'a;
 
@@ -201,6 +213,11 @@ where
     fn get_by_name(&self, name: &str) -> Result<Self::Attribute<'_>, AccessByNameError> {
         let elem = InMemDicomObject::element_by_name(*self, name)?;
         Ok(elem.value())
+    }
+
+    #[inline]
+    fn at(&self, selector: impl Into<AttributeSelector>) -> Result<Self::LeafAttribute<'_>, AtAccessError> {
+        self.value_at(selector)
     }
 }
 
@@ -3665,7 +3682,17 @@ mod tests {
                 ))
                 .unwrap(),
                 &PrimitiveValue::from(1_u16).into(),
-            )
+            );
+
+            // same result when using `DicomObject::at`
+            assert_eq!(
+                DicomObject::at(&obj, (
+                    tags::SEQUENCE_OF_ULTRASOUND_REGIONS,
+                    tags::REGION_SPATIAL_FORMAT
+                ))
+                .unwrap(),
+                &PrimitiveValue::from(1_u16).into(),
+            );
         }
     }
 
