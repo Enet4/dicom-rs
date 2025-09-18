a52d462c85b28d02a0bd8f4d3ef1243846ae3710
Nate Richman <naterichman@flywheel.io>
2025-09-18 11:44:35 -0500

    MAIN: More review changes
    * Update MSRV in workflow to 1.75.0
    * Change `tls` feature flag to `sync-tls` to clarify difference with `async-tls`
    * Install default cryptoprovider in tests

--- ul/src/association/server.rs
+++ ul/src/association/server.rs
@@ -33,7 +33,7 @@ use super::{
     Error, Result
 };
 
-#[cfg(feature = "tls")]
+#[cfg(feature = "sync-tls")]
 pub type TlsStream = rustls::StreamOwned<rustls::ServerConnection, std::net::TcpStream>;
 #[cfg(feature = "async-tls")]
 pub type AsyncTlsStream = tokio_rustls::server::TlsStream<tokio::net::TcpStream>;
@@ -290,7 +290,7 @@ pub struct ServerAssociationOptions<'a, A> {
     /// Options for the underlying TCP socket
     socket_options: SocketOptions,
     /// TLS configuration for the underlying TCP socket
-    #[cfg(feature = "tls")]
+    #[cfg(feature = "sync-tls")]
     tls_config: Option<std::sync::Arc<rustls::ServerConfig>>,
 }
 
@@ -307,7 +307,7 @@ impl Default for ServerAssociationOptions<'_, AcceptAny> {
             strict: true,
             promiscuous: false,
             socket_options: SocketOptions::default(),
-            #[cfg(feature = "tls")]
+            #[cfg(feature = "sync-tls")]
             tls_config: None,
         }
     }
@@ -360,7 +360,7 @@ where
             promiscuous,
             ae_access_control: _,
             socket_options,
-            #[cfg(feature = "tls")]
+            #[cfg(feature = "sync-tls")]
             tls_config
         } = self;
 
@@ -375,7 +375,7 @@ where
             strict,
             promiscuous,
             socket_options,
-            #[cfg(feature = "tls")]
+            #[cfg(feature = "sync-tls")]
             tls_config
         }
     }
@@ -460,7 +460,7 @@ where
     }
 
     /// Set the TLS configuration for the underlying TCP socket
-    #[cfg(feature = "tls")]
+    #[cfg(feature = "sync-tls")]
     pub fn tls_config(mut self, config: impl Into<std::sync::Arc<rustls::ServerConfig>>) -> Self {
         self.tls_config = Some(config.into());
         self
@@ -666,7 +666,7 @@ where
     }
 
     /// Negotiate an association with the given TCP stream using TLS.
-    #[cfg(feature = "tls")]
+    #[cfg(feature = "sync-tls")]
     pub fn establish_tls(&self, socket: TcpStream) -> Result<ServerAssociation<TlsStream>> {
         ensure!(
             !self.abstract_syntax_uids.is_empty() || self.promiscuous,
