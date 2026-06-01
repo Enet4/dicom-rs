4933efbd8d54565055a45324ef4693735fade28c
Nate Richman <naterichman@flywheel.io>
2026-06-01 12:11:50 -0500

    MAIN: Remove unused imports

--- ul/tests/association.rs
+++ ul/tests/association.rs
@@ -1,10 +1,8 @@
 use dicom_dictionary_std::uids::{self, VERIFICATION};
 #[cfg(feature = "sync-tls")]
-use dicom_ul::association::Association;
 use dicom_ul::{ClientAssociationOptions, Pdu, ServerAssociationOptions};
 use rstest::rstest;
 #[cfg(feature = "sync-tls")]
-use std::sync::Arc;
 use std::time::Instant;
 
 type Result<T, E = Box<dyn std::error::Error + Send + Sync + 'static>> = std::result::Result<T, E>;
